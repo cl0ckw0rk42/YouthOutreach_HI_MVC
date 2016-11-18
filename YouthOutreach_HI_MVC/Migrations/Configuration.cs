5341c10d758d75170420106cf6682bbe2ab3ade2
Daniel Boggs <dgboggs001@gmail.com>
2016-11-18 12:43:55 -1000

    Roles implemented with simple canEdit for database entries

--- YouthOutreach_HI_MVC/Migrations/Configuration.cs
+++ YouthOutreach_HI_MVC/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace YouthOutreach_HI_MVC.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using YouthOutreach_HI_MVC.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<YouthOutreach_HI_MVC.Models.ApplicationDbContext>
     {
@@ -13,8 +15,38 @@ namespace YouthOutreach_HI_MVC.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        bool AddUserAndRole(YouthOutreach_HI_MVC.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+
+            ir = rm.Create(new IdentityRole("canEdit"));
+
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            //testing giving updated permissions to my own entry
+            var user = new ApplicationUser()
+            {
+                UserName = "user1@contoso.com",
+            };
+
+            //hard-coded password REMOVED but standard basic boring one
+           //ir = um.Create(user, "REMOVED");
+
+            if (ir.Succeeded == false)               
+                return ir.Succeeded;
+
+            ir = um.AddToRole(user.Id, "canEdit");
+            return ir.Succeeded;
+        }
+
         protected override void Seed(YouthOutreach_HI_MVC.Models.ApplicationDbContext context)
         {
+            //adding authority here
+            AddUserAndRole(context);
+
             context.Donations.AddOrUpdate(p => p.Name,
                 new Donations
                 {
