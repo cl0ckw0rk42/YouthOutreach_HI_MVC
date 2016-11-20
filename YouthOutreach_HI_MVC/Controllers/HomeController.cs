0f560a44ff7c91f45a0e9e1b4ecf84ec50a0c0d4
Daniel Boggs <dgboggs001@gmail.com>
2016-11-20 04:02:40 -1000

    Emailing framework in, need to add specifics to env variables

--- YouthOutreach_HI_MVC/Controllers/HomeController.cs
+++ YouthOutreach_HI_MVC/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YouthOutreach_HI_MVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace YouthOutreach_HI_MVC.Controllers
 {
@@ -25,6 +29,32 @@ namespace YouthOutreach_HI_MVC.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("dgboggs001@gmail.com")); //replace with actual target !@#$
+                message.Subject = "Test from contact form";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
             return View();
         }
     }
